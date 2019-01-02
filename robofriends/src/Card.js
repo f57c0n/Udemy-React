ecaf05848fe21e7545ef7d050aa93a5ebe4b33ba
jj <inbox4development@gmail.com>
2019-01-01 19:05:01 -0500

    Update

---
+++ robofriends/src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = () => {
+    return (
+        <div>
+            <img alt='robots' src='' />
+            <div>
+                <h2>John Doe</h2>
+                <p>email address</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
